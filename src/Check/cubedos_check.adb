c47ec50b47cef1255d8497ce9e33130f931036fe
Peter Chapin <peter@pchapin.org>
2018-07-24 14:44:07 -0400

    Moved the CFDP implemenation to the new DTN repository.

--- src/Check/cubedos_check.adb
+++ src/Check/cubedos_check.adb
@@ -11,14 +11,12 @@
 --      Randolph Center, VT 05061 USA
 --      CBrandon@vtc.vsc.edu
 ---------------------------------------------------------------------------
-with CubedOS.CFDP.Internals.Check;
 with CubedOS.Lib.Bounded_Strings.Check;
 with CubedOS.Lib.CRC.Check;
 with CubedOS.Lib.XDR.Check;
 
 procedure CubedOS_Check is
 begin
-   CubedOS.CFDP.Internals.Check.Run_Tests;
    CubedOS.Lib.Bounded_Strings.Check.Run_Tests;
    CubedOS.Lib.CRC.Check.Run_Tests;
    CubedOS.Lib.XDR.Check.Run_Tests;
