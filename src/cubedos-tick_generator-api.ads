4300107f39f7af539499c9f678abe668d8b2bb54
Peter Chapin <chapinp@acm.org>
2019-10-20 16:27:53 -0400

    Set up the GNATdoc documentation generator. Added documentation to the Tick_Generator API package.

--- src/cubedos-tick_generator-api.ads
+++ src/cubedos-tick_generator-api.ads
@@ -17,17 +17,40 @@ use Message_Manager;
 
 package CubedOS.Tick_Generator.API is
 
+   -- Specifies the kinds of messages that can be sent to or received from the tick generator.
+   -- @value Relative_Request A request for a tick using a time relative to when the request
+   --   is made (e. g., "5 seconds from now").
+   -- @value Absolute_Request A request for a tick using an absolute time that is independent
+   --   from when the request is made (e. g., "13:25:30 on January 1, 2021").
+   -- @value Tick_Reply The message type used for tick messages returned to client modules.
+   -- @value Cancel_Request A request to cancel a previously defined series.
    type Message_Type is
      (Relative_Request,
       Absolute_Request,
       Tick_Reply,
       Cancel_Request);
 
+   -- Two types of tick "series" are supported.
+   -- @value One_Shot A single Tick_Reply message will be sent. The series is automatically
+   --   canceled after that message.
+   -- @value Periodic Tick_Reply messages will be sent periodically until explicitly canceled.
    type Series_Type is (One_Shot, Periodic);
+
+   -- Each tick series has an ID number used to identify it in subsequent messages.
    type Series_ID_Type is range 1 .. Natural'Last;
+
+   -- Tick_Reply messages contain a counter that indicates how many tick messages have been sent
+   -- on a particular series. Although Tick_Reply messages start with a count of 1, the value
+   -- 0 is allowed here so that variables that count the number of tick messages sent/received
+   -- can be initialized in a reasonable way.
    type Series_Count_Type is range 0 .. Natural'Last;
 
    -- Return a message for requesting ticks that are scheduled relative to the current time.
+   -- @param Tick_Interval The time interval until the first tick message (and between tick
+   --   messages in the case of a periodic series).
+   -- @param Request_Type Indicates if a One_Shot or Periodic series is requested.
+   -- @param Series_ID The ID number for the series (selected by the caller and thus only
+   --   meaningful to the requesting module).
    function Relative_Request_Encode
      (Sender_Domain : Domain_ID_Type;
       Sender        : Module_ID_Type;
@@ -39,6 +62,10 @@ package CubedOS.Tick_Generator.API is
      with Global => null;
 
    -- Return a message for requesting a single tick message at a specific, absolute time.
+   -- @param Tick_Time The time when the tick message is requested. Absolute requests are always
+   --   one shot requests so no Request_Type paramter is needed.
+   -- @param Series_ID The ID number of the series (selected by the caller and thus only
+   --   meaningful to the requesting module).
    function Absolute_Request_Encode
      (Sender_Domain : Domain_ID_Type;
       Sender     : Module_ID_Type;
@@ -49,6 +76,12 @@ package CubedOS.Tick_Generator.API is
      with Global => null;
 
    -- The tick messages themselves.
+   -- @param Series_ID The ID number of the series to distinguish this tick message from those
+   --   of other series being sent to the same module.
+   -- @param Count The count for this tick reply. Counts start at one and increment for each
+   --   message sent. The counts are scoped by their series (i. e., every series has its own
+   --   count sequence). Should the count reach Series_Count_Type'Last, the series is canceled
+   --   after a Tick_Reply with that count is sent.
    function Tick_Reply_Encode
      (Receiver_Domain : Domain_ID_Type;
       Receiver   : Module_ID_Type;
@@ -58,6 +91,9 @@ package CubedOS.Tick_Generator.API is
       Priority   : System.Priority := System.Default_Priority) return Message_Record;
 
    -- Return a message for canceling previous tick requests (of any kind).
+   -- @param Series_ID The ID number of the series being canceled. If the series does not
+   --   exist or has already been canceled or removed (due to being a one shot series that has
+   --   already fired), there is no effect.
    function Cancel_Request_Encode
      (Sender_Domain : Domain_ID_Type;
       Sender     : Module_ID_Type;
@@ -80,6 +116,8 @@ package CubedOS.Tick_Generator.API is
      (Message.Receiver = ID and Message.Message_ID = Message_Type'Pos(Cancel_Request));
 
 
+   -- Decode Relative_Request messages. See Relative_Request_Encode for information about the
+   -- parameters.
    procedure Relative_Request_Decode
      (Message       : in  Message_Record;
       Tick_Interval : out Ada.Real_Time.Time_Span;
@@ -91,6 +129,8 @@ package CubedOS.Tick_Generator.API is
        Pre => Is_Relative_Request(Message),
        Depends => ((Tick_Interval, Request_Type, Series_ID, Decode_Status) => Message);
 
+   -- Decode Absolute_Request messages. See Absolute_Request_Encode for information about the
+   -- parameters.
    procedure Absolute_Request_Decode
      (Message   : in  Message_Record;
       Tick_Time : out Ada.Real_Time.Time;
@@ -101,6 +141,7 @@ package CubedOS.Tick_Generator.API is
        Pre => Is_Absolute_Request(Message),
        Depends => ((Tick_Time, Series_ID, Decode_Status) => Message);
 
+   -- Decode Tick_Reply messages. See Tick_Reply_Encode for information about the parameters.
    procedure Tick_Reply_Decode
      (Message   : in  Message_Record;
       Series_ID : out Series_ID_Type;
@@ -111,6 +152,8 @@ package CubedOS.Tick_Generator.API is
        Pre => Is_Tick_Reply(Message),
        Depends => ((Series_ID, Count, Decode_Status) => Message);
 
+   -- Decode Cancel_Request messages. See Cancel_Request_Encode for information about the
+   -- parameters.
    procedure Cancel_Request_Decode
      (Message   : in  Message_Record;
       Series_ID : out Series_ID_Type;
