5d4cdc005a9dd2eea77e4d0f48de46ca7d60ed49
Peter Chapin <chapinp@acm.org>
2021-03-14 15:44:39 -0400

    Added a rules file for GNATcheck.

--- src/cubedos-lib-vectors3.adb
+++ src/cubedos-lib-vectors3.adb
@@ -14,7 +14,7 @@ package body CubedOS.Lib.Vectors3 is
      new Ada.Numerics.Generic_Elementary_Functions(Float_Type);
    use Float_Type_Elementary_Functions;
 
-   function Cross(Left, Right : Vector3) return Vector3 is
+   function Cross(Left, Right : in Vector3) return Vector3 is
       New_X : Float_Type;
       New_Y : Float_Type;
       New_Z : Float_Type;
@@ -26,7 +26,7 @@ package body CubedOS.Lib.Vectors3 is
    end Cross;
 
 
-   function Magnitude(V : Vector3) return Float_Type is
+   function Magnitude(V : in Vector3) return Float_Type is
       Result : Float_Type;
    begin
       Result := Sqrt( (V.X * V.X) + (V.Y * V.Y) + (V.Z * V.Z) );
@@ -34,7 +34,7 @@ package body CubedOS.Lib.Vectors3 is
    end Magnitude;
 
 
-   function Unit(V : Vector3) return Vector3 is
+   function Unit(V : in Vector3) return Vector3 is
       Length : constant Float_Type := Magnitude(V);
    begin
       return ( X => V.X/Length, Y => V.Y/Length, Z => V.Z/Length );
