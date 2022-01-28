3d936da10b373b7752cc23e9f5e6d35ada559fd6
sevan golnazarian <sgolnaza@gmail.com>
2022-01-28 10:23:26 -0500

    fix spelling of the Priority aspect and correct syntax errors

--- src/modules/cubedos-publish_subscribe_server-messages.ads
+++ src/modules/cubedos-publish_subscribe_server-messages.ads
@@ -15,12 +15,12 @@ with System;
 package CubedOS.Publish_Subscribe_Server.Messages
   with
     Abstract_State => Database,
-    Initializes => (Database, Message_Loop),
+    Initializes => (Database, Message_Loop)
 is
 
    task type Message_Loop
      with Global => (In_Out => (Database, Message_Manager.Mailboxes)),
-        Prioriry => System.Default_Priotity;
+        Priority => System.Default_Priority
    is
       -- pragma Storage_Size(4 * 1024);
       -- pragma Priority(System.Default_Priority);
