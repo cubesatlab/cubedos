ea28c32b8fef77675173be2d636561afa73f20f3
Peter Chapin <pchapin@vtc.edu>
2022-03-12 15:40:58 -0500

    Defined requirements for the CubedOS Log Server module. Implemented support for different log levels.

--- src/modules/cubedos-log_server-api.ads
+++ src/modules/cubedos-log_server-api.ads
@@ -1,7 +1,7 @@
 --------------------------------------------------------------------------------
 -- FILE   : cubedos-log_server-api.ads
 -- SUBJECT: Specification of the logger's API package
--- AUTHOR : (C) Copyright 2021 by Vermont Technical College
+-- AUTHOR : (C) Copyright 2022 by Vermont Technical College
 --
 -- All the subprograms in this package must be task safe. They can be simultaneously called
 -- from multiple tasks. If possible, make every subprogram here a pure function.
@@ -14,34 +14,43 @@ with System;
 
 package CubedOS.Log_Server.API is
 
-   -- Define data types here that are only needed in this API (such as status codes). Data
-   -- types that might also be used in the implementation should be in the top level package.
-   type Status_Type is (Success, Failure);
+   -- This type is not needed right now. It is intended to be used in replies to indicate success or failure
+   -- of a request. However, the Log Server never sends any reply messages. Maybe later?
+   --type Status_Type is (Success, Failure);
 
    -- The maximum size of a log message.
+   -- The other types here are for convenience only, but notice that Log_Message_Type is always 128 characters.
+   -- In contrast, if a short string is sent to Log_Message, only a short string is encoded.
    Maximum_Log_Message_Size : constant := 128;
    subtype Log_Message_Size_Type is Natural range 0 .. Maximum_Log_Message_Size;
    subtype Log_Message_Index_Type is Positive range 1 .. Maximum_Log_Message_Size;
+   subtype Log_Message_Type is String(1 .. Maximum_Log_Message_Size);
 
-   -- TODO: What messages should this package really send/receive?
+   -- Different log levels to indicate degree of "seriousness" of a message.
+   type Log_Level_Type is (Debug, Informational, Warning, Error, Critical);
+
    type Message_Type is
      (Log_Text);
 
-
    -- Convenience procedure that creates and sends a log message.
    procedure Log_Message
-     (Sender_Domain : Domain_ID_Type; Sender : in Module_ID_Type; Text : in String);
+     (Sender_Domain : in Domain_ID_Type;
+      Sender        : in Module_ID_Type;
+      Log_Level     : in Log_Level_Type;
+      Text          : in String)
+     with Pre => Text'Length <= Maximum_Log_Message_Size;
 
 
    function Log_Text_Encode
      (Sender_Domain : Domain_ID_Type;
       Sender     : Module_ID_Type;
       Request_ID : Request_ID_Type;
+      Log_Level  : Log_Level_Type;
       Text       : String;
       Priority   : System.Priority := System.Default_Priority) return Message_Record
      with
        Global => null,
-       Pre => Text'Length <= Maximum_Log_Message_Size;
+       Pre    => Text'Length <= Maximum_Log_Message_Size;
 
 
    function Is_A_Log_Text(Message : Message_Record) return Boolean is
@@ -49,13 +58,14 @@ package CubedOS.Log_Server.API is
 
 
    procedure Log_Text_Decode
-     (Message : in  Message_Record;
-      Text : out String;
-      Size : out Log_Message_Size_Type;
+     (Message   : in  Message_Record;
+      Log_Level : out Log_Level_Type;
+      Text      : out String;
+      Size      : out Log_Message_Size_Type;
       Decode_Status : out Message_Status_Type)
    with
-     Global => null,
-     Pre => Is_A_Log_Text(Message) and Text'Length >= Maximum_Log_Message_Size,
-     Depends => (Text =>+ Message, (Size, Decode_Status) => Message);
+     Global  => null,
+     Pre     => Is_A_Log_Text(Message) and Text'Length >= Maximum_Log_Message_Size,
+     Depends => (Text =>+ Message, (Log_Level, Size, Decode_Status) => Message);
 
 end CubedOS.Log_Server.API;
--- src/modules/cubedos-log_server-messages.ads
+++ src/modules/cubedos-log_server-messages.ads
@@ -1,28 +1,23 @@
 --------------------------------------------------------------------------------
 -- FILE   : cubedos.log_server-messages.ads
 -- SUBJECT: Specification of a package that implements the main part of the module.
--- AUTHOR : (C) Copyright 2021 by Vermont Technical College
+-- AUTHOR : (C) Copyright 2022 by Vermont Technical College
 --
 --------------------------------------------------------------------------------
 pragma SPARK_Mode(On);
 pragma Profile(Ravenscar);
 pragma Partition_Elaboration_Policy(Sequential);
 
+with Ada.Text_IO;
 with System;
 
 package CubedOS.Log_Server.Messages is
 
    task Message_Loop
-     with Global => (In_Out => Message_Manager.Mailboxes)
+     with Global => (In_Out => (Ada.Text_IO.File_System, Message_Manager.Mailboxes))
    is
       -- pragma Storage_Size(4 * 1024);
       pragma Priority(System.Default_Priority);
    end Message_Loop;
-
-   pragma Annotate
-     (GNATprove,
-      Intentional,
-      "multiple tasks might queue on protected entry",
-      "Every module has a unique ID");
 
 end CubedOS.Log_Server.Messages;
