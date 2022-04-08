d4cdb821af88ae1709944333bb3fa9ec45be2040
sevan golnazarian <sgolnaza@gmail.com>
2022-04-08 14:03:30 -0400

    comment out unused modules in Name_Resolver and adjust Module_Count in Message_Manager

--- samples/Echo/name_resolver.ads
+++ samples/Echo/name_resolver.ads
@@ -9,15 +9,16 @@ with Message_Manager; use Message_Manager;
 package Name_Resolver is
 
     -- Core Modules
-    Log_Server               : constant Message_Address := (0, 2);
-    Publish_Subscribe_Server : constant Message_Address := (0, 3);
-    Time_Server              : constant Message_Address := (0,4);
-    Event_Server             : constant Message_Address := (0,5);
-    File_Server              : constant Message_Address := (0,6);
-    Table_Server             : constant Message_Address := (0,7);
+    -- Log_Server               : constant Message_Address := (0, 2);
+    -- Publish_Subscribe_Server : constant Message_Address := (0, 3);
+    -- Time_Server              : constant Message_Address := (0,4);
+    -- Event_Server             : constant Message_Address := (0,5);
+    -- File_Server              : constant Message_Address := (0,6);
+    -- Table_Server             : constant Message_Address := (0,7);
+    -- Interpreter              : constant Message_Address := (0,10);
 
     -- Application-Specific Modules
-    Echo_Client             : constant Message_Address := (0,8);
-    Echo_Server             : constant Message_Address := (0,9);
-    
+    Echo_Client             : constant Message_Address := (0,1);
+    Echo_Server             : constant Message_Address := (0,2);
+
 end Name_Resolver;
