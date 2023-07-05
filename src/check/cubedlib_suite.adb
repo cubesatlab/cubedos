6a64b5be3267cc5ae6d4d7ae2e7cd9d49f9863aa
Eric Edlund <41064585+Eric-Edlund@users.noreply.github.com>
2023-07-05 15:37:47 -0400

    Add SPARK Bounded Queue implementation (#36)
    It's not fully proven, but has been tested. A simple
    fixed size queue with a circular buffer. It uses pointers.

--- src/check/cubedlib_suite.adb
+++ src/check/cubedlib_suite.adb
@@ -14,6 +14,7 @@ with CubedOS.Lib.Bounded_Strings.Check;
 with Check_Trivial;
 with Check_Lib_CRC;
 with Check_Lib_XDR;
+with Check_Bounded_Queue;
 
 package body CubedLib_Suite is
    use AUnit.Test_Suites;
@@ -28,6 +29,7 @@ package body CubedLib_Suite is
    Test_1 : aliased Check_Lib_CRC.Lib_CRC_Test;
    Test_2 : aliased Check_Lib_XDR.Lib_XDR_Test;
    Test_3 : aliased CubedOS.Lib.Bounded_Strings.Check.Lib_Bounded_Strings_Test;
+   Test_4 : aliased Check_Bounded_Queue.Queue_Test;
 
    -- Function to return an access to the configured suite
    function Suite return Access_Test_Suite is
@@ -36,6 +38,7 @@ package body CubedLib_Suite is
       Add_Test(Suite_Object'Access, Test_1'Access);
       Add_Test(Suite_Object'Access, Test_2'Access);
       Add_Test(Suite_Object'Access, Test_3'Access);
+      Add_Test(Suite_Object'Access, Test_4'Access);
       return Suite_Object'Access;
    end Suite;
 
