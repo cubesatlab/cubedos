ee027ef51ff2166d9a8df9d5a30e17c0a612511d
Peter Chapin <2094217+pchapin@users.noreply.github.com>
2024-06-28 08:02:59 -0400

    Added the Log Server module to the STM43F4 sample program.

--- samples/STM32F4/name_resolver.ads
+++ samples/STM32F4/name_resolver.ads
@@ -8,13 +8,14 @@ with Message_Manager; use Message_Manager;
 
 package Name_Resolver is
 
-    -- Core Modules
-    Publish_Subscribe_Server : constant Message_Address := (0, 1);
-    Time_Server              : constant Message_Address := (0, 2);
+   -- Core Modules
+   Log_Server               : constant Message_Address := (0, 1);
+   Publish_Subscribe_Server : constant Message_Address := (0, 2);
+   Time_Server              : constant Message_Address := (0, 3);
 
-    -- Application-Specific Modules
-    LED_Driver    : constant Message_Address := (0, 3);
-    Button_Driver : constant Message_Address := (0, 4);
-    Control       : constant Message_Address := (0, 5);
+   -- Application-Specific Modules
+   LED_Driver    : constant Message_Address := (0, 4);
+   Button_Driver : constant Message_Address := (0, 5);
+   Control       : constant Message_Address := (0, 6);
 
 end Name_Resolver;
